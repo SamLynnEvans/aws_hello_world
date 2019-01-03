1a326d888689c3f28fb55a66d587a50462ba2f78
Ubuntu <ubuntu@ip-172-31-16-82.us-east-2.compute.internal>
2019-01-03 20:22:30 +0000

    edited host name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'http://ec2-18-188-69-127.us-east-2.compute.amazonaws.com',
+      host: 'ec2-18-188-69-127.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/ubuntu_one.pem',
       ref: 'origin/master',
       repo: 'https://github.com/SamLynnEvans/aws_hello_world',
